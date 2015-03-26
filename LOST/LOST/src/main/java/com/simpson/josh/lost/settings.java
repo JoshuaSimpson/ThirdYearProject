f40a9cf0a052fef89542edad5f3aa44958585ef3
IvanMcBastard <k1209590@kcl.ac.uk>
2015-03-26 01:33:25 +0000

    JSON plays well with AccessPoints - looks like a backend issue ( otherwise known as 'why the shit did I name so short-sightedly?' )

--- LOST/LOST/src/main/java/com/simpson/josh/lost/settings.java
+++ LOST/LOST/src/main/java/com/simpson/josh/lost/settings.java
@@ -8,12 +8,17 @@ import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
-import org.apache.http.client.HttpClient;
+import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.text.DateFormat;
 import java.util.Date;
 
@@ -65,11 +70,37 @@ public class settings extends ActionBarActivity {
 
                 String dateString = DateFormat.getInstance().format(d.getTime());
 
+                JSONObject jsonobj = new JSONObject();
+                try {
+                    jsonobj.put("mac", "mobileMac");
+                    jsonobj.put("location", "Doge Central");
+                    jsonobj.put("time", dateString);
+                    jsonobj.put("notes", "lol");
+                } catch (JSONException j) {
+                    j.printStackTrace();
+                }
 
-                HttpClient client = new DefaultHttpClient();
 
-                HttpPost poster = new HttpPost("http://178.62.73.203/locations");
+                DefaultHttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppostreq = new HttpPost("http://178.62.73.203/access_points.json");
 
+                StringEntity se = null;
+                try {
+                    se = new StringEntity(jsonobj.toString());
+                } catch (UnsupportedEncodingException u) {
+                    u.printStackTrace();
+                }
+
+                se.setContentType("application/json;charset=UTF-8");
+                se.setContentEncoding(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+
+                httppostreq.setEntity(se);
+
+                try {
+                    HttpResponse httpResponse = httpclient.execute(httppostreq);
+                } catch (Exception c) {
+                    c.printStackTrace();
+                }
                 Log.d("IT WORKED", "Stuff");
             }
         }).start();
