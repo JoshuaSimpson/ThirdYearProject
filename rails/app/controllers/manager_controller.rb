a3f5805018f6be60fe640a62a8f29909bc416550
JoshuaSimpson <k1209590@kcl.ac.uk>
2015-03-29 01:15:54 +0000

    Now serves database file on request

--- rails/app/controllers/manager_controller.rb
+++ rails/app/controllers/manager_controller.rb
@@ -5,4 +5,8 @@ class ManagerController < ApplicationController
 		File.open(Rails.root.join('public', 'dbs', uploaded_stuff.original_filename), "wb") do |file| file.write(uploaded_stuff.read) 
 		end
 	end
+
+	def download
+		send_file '/home/rails/public/dbs/LOSTANDFOUND', :type=>"application/db"
+	end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -6,6 +6,7 @@ Myapp::Application.routes.draw do
   root to: 'home#index'
 
   post '/dbs', to: 'manager#upload'
+  get '/dbs/get', to: 'manager#download'
 
   resources :access_points, only: [:index, :show, :new, :create, :destroy, :edit, :patch]
 end
