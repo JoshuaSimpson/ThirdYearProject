061ae728771b52c4c0975855b958eda0b49ae6a7
JoshuaSimpson <k1209590@kcl.ac.uk>
2015-04-16 01:30:12 +0000

    Added Devise for user authentication

--- rails/app/controllers/home_controller.rb
+++ rails/app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
+  before_filter :authenticate_user!
   def index
   	@access_points = AccessPoint.find( :all, :order => "time DESC", :limit => 5)
 
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -1,4 +1,5 @@
 Myapp::Application.routes.draw do
+  devise_for :users
   resources :locations
 
   get "home/index"
