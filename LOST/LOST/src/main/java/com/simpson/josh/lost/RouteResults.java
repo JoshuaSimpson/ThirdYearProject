663633e4c32282a668d5a1e9695a1e70acc0ed06
Joshua Simpson <k1209590@kcl.ac.uk>
2015-04-24 18:16:14 +0100

    Removed duplicate steps in path and changed icon

--- LOST/LOST/src/main/java/com/simpson/josh/lost/RouteResults.java
+++ LOST/LOST/src/main/java/com/simpson/josh/lost/RouteResults.java
@@ -9,6 +9,8 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import java.util.LinkedList;
+
 // http://www.icons4android.com/iconset/29 - Thank them for the Icon Set.
 public class RouteResults extends Activity {
 
@@ -23,25 +25,31 @@ public class RouteResults extends Activity {
         String myString = "";
 
         // Attach to tablelayout, and inflate the table rows
+        LinkedList<String> alreadyDone = new LinkedList<String>();
+
         TableLayout table = (TableLayout) findViewById(R.id.resultsTable);
         for (int i = 0; i < path.length; i++) {
-            TableRow row = (TableRow) LayoutInflater.from(RouteResults.this).inflate(R.layout.inflatable_table_row, null);
-            ImageView iv = (ImageView) row.findViewById(R.id.statusIcon);
-            Log.d("Trying here", "Seriously");
+            if(!alreadyDone.contains(path[i]) && i < path.length) {
+                TableRow row = (TableRow) LayoutInflater.from(RouteResults.this).inflate(R.layout.inflatable_table_row, null);
+                ImageView iv = (ImageView) row.findViewById(R.id.statusIcon);
+                Log.d("Trying here", "Seriously");
 
-            // Set icon depending on the status
-            if (i == 0) {
-                iv.setImageResource(R.drawable.ic_en_route);
-            } else if (i == path.length - 1) {
-                iv.setImageResource(R.drawable.ic_fin);
-            } else {
-                iv.setImageResource(R.drawable.ic_action);
+                Log.d(path[i], path[path.length-1]);
+                // Set icon depending on the status
+                if (i == 0) {
+                    iv.setImageResource(R.drawable.ic_en_route);
+                } else if (i == path.length - 1 || path[i].equals(path[path.length-1])) {
+                    iv.setImageResource(R.drawable.ic_fin);
+                } else {
+                    iv.setImageResource(R.drawable.ic_action_expand);
+                }
+
+
+                TextView instructionText = (TextView) row.findViewById(R.id.instruction);
+                instructionText.setText(path[i]);
+                alreadyDone.add(path[i]);
+                table.addView(row);
             }
-
-
-            TextView instructionText = (TextView) row.findViewById(R.id.instruction);
-            instructionText.setText(path[i]);
-            table.addView(row);
         }
     }
 }
