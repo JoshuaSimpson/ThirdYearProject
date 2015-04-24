b2d504cba677c03ef9ebb60ece09b47fb298be73
Joshua Simpson <k1209590@kcl.ac.uk>
2015-04-24 18:17:13 +0100

    LoginDialog now sets up AccessKings and Eduroam correctly

--- LOST/LOST/src/main/java/com/simpson/josh/lost/LoginDialog.java
+++ LOST/LOST/src/main/java/com/simpson/josh/lost/LoginDialog.java
@@ -1,13 +1,19 @@
 package com.simpson.josh.lost;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiEnterpriseConfig;
+import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.TextView;
 
 
 public class LoginDialog extends Activity {
@@ -16,6 +22,7 @@ public class LoginDialog extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_dialog);
+
 
         Button close_button = (Button) findViewById(R.id.dismiss);
         close_button.setOnClickListener(new View.OnClickListener() {
@@ -27,13 +34,41 @@ public class LoginDialog extends Activity {
 
         Button setUp = (Button) findViewById(R.id.addWiFi);
         setUp.setOnClickListener(new View.OnClickListener() {
+
+            @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
             @Override
             public void onClick(View view) {
                 Log.d("6 and a half thousand"," fucking. Miles.");
 
-                
-                
-            }
+                TextView username = (TextView)findViewById(R.id.usernameBox);
+                TextView pwd = (TextView)findViewById(R.id.passwordBox);
+
+                WifiConfiguration wifiConfig = new WifiConfiguration();
+                WifiEnterpriseConfig epConfig = new WifiEnterpriseConfig();
+
+                WifiManager wfm = (WifiManager)getSystemService(Context.WIFI_SERVICE);
+
+                //Block to set up AccessKings
+                wifiConfig.SSID = "AccessKings";
+                wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.WPA_EAP);
+                wifiConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.IEEE8021X);
+                epConfig.setIdentity(username.getText().toString());
+                epConfig.setPassword(pwd.getText().toString());
+                epConfig.setEapMethod(WifiEnterpriseConfig.Eap.PEAP);
+
+                wifiConfig.enterpriseConfig = epConfig;
+
+                wfm.addNetwork(wifiConfig);
+
+                //Modify a little bit to set up eduroam
+                wifiConfig.SSID = "eduroam";
+                epConfig.setIdentity(username.getText().toString() + "@kclad.ds.kcl.ac.uk");
+                wifiConfig.enterpriseConfig = epConfig;
+
+                wfm.addNetwork(wifiConfig);
+
+                finish();
+                }
         });
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
--- LOST/LOST/src/main/java/com/simpson/josh/lost/settings.java
+++ LOST/LOST/src/main/java/com/simpson/josh/lost/settings.java
@@ -4,10 +4,12 @@ import android.app.DialogFragment;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 
@@ -21,6 +23,14 @@ public class settings extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button wifiSetup = (Button)findViewById(R.id.addWiFi);
+
+        if(Build.VERSION.SDK_INT < 18)
+        {
+            wifiSetup.setVisibility(View.GONE);
+            wifiSetup.setEnabled(false);
+        }
 
         locationBox = (CheckBox) findViewById(R.id.locationBox);
         wirelessBox = (CheckBox) findViewById(R.id.wirelessBox);
